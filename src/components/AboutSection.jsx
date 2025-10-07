b19075e79be80af9df7ca84c48fb74e2677cc8ac
Nevindi-BotCalm <nevind.l@botcalm.com>
2025-10-07 10:12:56 +0530

    About Section Mobile Responsiveness DONE

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -19,19 +19,20 @@ function AboutSection() {
         alt="Captain Mek Character"
         className="hidden md:block absolute left-[210px] top-[278px] w-[507px] h-[707px] object-contain opacity-100 rotate-0"
       />
-
       {/* Responsive Images */}
       <div className="block md:hidden relative">
-        <img
-          src={AboutSectionResponsive1}
-          alt="About Section Mobile 1"
-          className="w-full h-auto object-cover"
-        />
-
+        {/* Image 2 – goes behind & slightly lower */}
         <img
           src={AboutSectionResponsive2}
           alt="About Section Mobile 2"
-          className="absolute top-1/2 left-0 w-full h-auto object-cover"
+          className="w-full h-auto object-cover absolute top-[450px] left-0 z-0"
+        />
+
+        {/* Image 1 – stays in front */}
+        <img
+          src={AboutSectionResponsive1}
+          alt="About Section Mobile 1"
+          className="w-full h-auto object-cover relative z-10"
         />
       </div>
 
@@ -41,19 +42,17 @@ function AboutSection() {
           Captain Mek the First Avenger of meme coins wields $CMEK as a powerful
           shield, leading the crypto community to turn chaos into profit. More
           than just a meme, it combines internet culture and blockchain
-          technology to empower everyday investors. 
-          <br/>
-          <br/>
-          The project is built on
-          three key pillars: Mek’s Shield, an AI-driven defense against scams
-          and MEV; Mek’s Vault, a staking fortress offering high APY; and Mek’s
-          War Room, a precision meme coin signal hub to help investors stay
-          ahead. 
-          <br/>
-          <br/>
-          
-          No villains, no drama only heroes, loot, and glory. Grab your
-          $CMEK and join the revolution to conquer the memecoin universe!k
+          technology to empower everyday investors.
+          <br />
+          <br />
+          The project is built on three key pillars: Mek’s Shield, an AI-driven
+          defense against scams and MEV; Mek’s Vault, a staking fortress
+          offering high APY; and Mek’s War Room, a precision meme coin signal
+          hub to help investors stay ahead.
+          <br />
+          <br />
+          No villains, no drama only heroes, loot, and glory. Grab your $CMEK
+          and join the revolution to conquer the memecoin universe!k
         </p>
       </div>
     </div>
